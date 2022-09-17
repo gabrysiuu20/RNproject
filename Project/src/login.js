1df7d40de31e0f5be78ca1100705aaa31c28ff00
gabrysiuu20 <gabrylosow1999@gmail.com>
2022-09-17 15:20:01 +0200

    added the ability to log in

--- Project/src/login.js
+++ Project/src/login.js
@@ -20,23 +20,62 @@ import {
 } from 'react-native';
 
 
-export const Login = ({ navigation }) => {
-    return (
-        <MainContainer>
-            <HomeTitle style = {styles.titleFont}>Login</HomeTitle>
-            <InputBox>
-                <TextInput style={styles.mainFont} placeholder='Username' placeholderTextColor='white'/>
-            </InputBox>
-            <InputBox>
-                <TextInput style={styles.mainFont} placeholder='Password' placeholderTextColor='white' secureTextEntry={true}/>
-            </InputBox>
-            <Bttn>
-                <Text style = {styles.bttnFont}>LOGIN</Text>
-            </Bttn>
-            <Bttn
-                onPress = {() => navigation.navigate('Register')} 
-            ><Text style = {styles.bttnFont}>Don't have an account? Sign Up</Text></Bttn>
-        </MainContainer>
-    )
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+        }
+    }
+
+    loginAttempt = async () => {
+        this.setState({ loading: true })
+        fetch('http://10.0.2.2:8080/api/auth/signin', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+        .then((res) => {
+            if (!res.ok){
+                console.error(res.status.toString());
+            }
+        });
+    }
+
+    toRegisterPage = async () => {
+        this.props.navigation.navigate('Register');
+    }
+
+
+    render(){
+        return (
+            <MainContainer>
+                <HomeTitle style = {styles.titleFont}>Login</HomeTitle>
+                <InputBox>
+                    <TextInput style={styles.mainFont} placeholder='Email' placeholderTextColor='white' onChangeText={email=>this.setState({ email })}/>
+                </InputBox>
+                <InputBox>
+                    <TextInput style={styles.mainFont} placeholder='Password' placeholderTextColor='white' secureTextEntry={true} onChangeText={password=>this.setState({ password })}/>
+                </InputBox>
+                <Bttn
+                    onPress = {() => this.loginAttempt()}
+                >
+                    <Text style = {styles.bttnFont}>LOGIN</Text>
+                </Bttn>
+                <Bttn
+                    onPress = {() => this.toRegisterPage()} 
+                ><Text style = {styles.bttnFont}>Don't have an account? Sign Up</Text></Bttn>
+            </MainContainer>
+        )
+    }
+    
 }
 
--- Project/src/register.js
+++ Project/src/register.js
@@ -18,9 +18,10 @@ import {
     View,
     TextInput,
   } from 'react-native';
+import { user } from '../models';
+import { ThemeConsumer } from 'styled-components';
 
-const User = require("../models/user.model")
-
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 export class Register extends React.Component {
     constructor(props) {
@@ -33,6 +34,7 @@ export class Register extends React.Component {
             password: '',
             confirmPassword: '',
             loading: false,
+            errorMessage: [],
         }
     }
     
@@ -46,38 +48,66 @@ export class Register extends React.Component {
         // input validation
         if (this.state.password.length == 0) {
           errorFlag = true;
-          this.setState({ passwordErrorMessage: "Password is required field"});
-        } else if (this.state.password.length < 8 ||  this.state.password.length > 20) {
+          this.state.errorMessage.push("Password is required field");
+        } else if (this.state.password.length < 8 || this.state.password.length > 20) {
           errorFlag = true;
-          this.setState({ passwordErrorMessage: "Password should be min 8 char and max 20 char"});
+          this.state.errorMessage.push("Password should be min 8 char and max 20 char");
         } else if (this.state.password !==  this.state.confirmPassword ) {
           errorFlag = true;
-          this.setState({ passwordErrorMessage: "Passwoad and confirm password should be same."});
+          this.state.errorMessage.push("Password and confirm password should be same.");
         }
         
         if (this.state.confirmPassword.length == 0) {
           errorFlag = true;
-          this.setState({ confirmPasswordErrorMessage: "Confirm Password is required field"});
-        } else if (this.state.confirmPassword.length < 8 ||  this.state.confirmPassword.length > 20) {
-          errorFlag = true;
-          this.setState({ confirmPasswordErrorMessage: "Password should be min 8 char and max 20 char"});
+          this.state.errorMessage.push("Confirm Password is required field");
         }
+        
 
         if (isNaN(this.state.phone)) {
             errorFlag = true;
-            this.setState({ phoneErrorMessage: "Phone should have consist of digits"});
+            this.state.errorMessage.push("Phone should have consist of digits");
+        }
+
+        if (!emailRegex.test(this.state.email)){
+            errorFlag = true;
+            this.state.errorMessage.push("That's not email format");
         }
        
         if (errorFlag) {
-            console.log("errorFlag");
+            console.error(this.state.errorMessage.join('\n'));
             
             /** Call Your API */
         } else {
             this.setState({ loading: false });
             console.log(`${this.state.forename} ${this.state.surname} ${this.state.email} ${this.state.phone} ${this.state.password} ${this.state.confirmPassword}`);
-            this.props.navigation.navigate('Home');
+            fetch('http://10.0.2.2:8080/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    forename: this.state.forename,
+                    surname: this.state.surname,
+                    email: this.state.email,
+                    phone: this.state.phone,
+                    password: this.state.password,
+                    confirmPassword: this.state.confirmPassword,
+                    roles: [
+                        "user"
+                    ]
+                })
+            })
+            .then((res) => {
+                if (!res.ok){
+                    console.error(res.status.toString());
+                }else{
+                    this.props.navigation.navigate('Home');
+                }
+            });
         }
     }
+            
 
     
             
